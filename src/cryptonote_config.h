6e35c65e3a350534072e160049227be5c28d9eac
spackle-xmr <93713512+spackle-xmr@users.noreply.github.com>
2024-06-02 17:06:49 -0500

    Revert "Replace Testnet"
    This reverts commit 55e5830e9643ae09c7f6c82f7292e015797346f0.

--- src/cryptonote_config.h
+++ src/cryptonote_config.h
@@ -264,14 +264,14 @@ namespace config
     uint64_t const CRYPTONOTE_PUBLIC_ADDRESS_BASE58_PREFIX = 53;
     uint64_t const CRYPTONOTE_PUBLIC_INTEGRATED_ADDRESS_BASE58_PREFIX = 54;
     uint64_t const CRYPTONOTE_PUBLIC_SUBADDRESS_BASE58_PREFIX = 63;
-    uint16_t const P2P_DEFAULT_PORT = 28085;
-    uint16_t const RPC_DEFAULT_PORT = 28086;
-    uint16_t const ZMQ_RPC_DEFAULT_PORT = 28087;
+    uint16_t const P2P_DEFAULT_PORT = 28080;
+    uint16_t const RPC_DEFAULT_PORT = 28081;
+    uint16_t const ZMQ_RPC_DEFAULT_PORT = 28082;
     boost::uuids::uuid const NETWORK_ID = { {
-        0x73 ,0x70, 0x61, 0x63 , 0x6B, 0x6C , 0x65, 0x78, 0x6D, 0x72, 0xFF, 0x5C, 0xA1, 0xAB, 0x1E, 0xFF
+        0x12 ,0x30, 0xF1, 0x71 , 0x61, 0x04 , 0x41, 0x61, 0x17, 0x31, 0x00, 0x82, 0x16, 0xA1, 0xA1, 0x11
       } }; // Bender's daydream
     std::string const GENESIS_TX = "013c01ff0001ffffffffffff03029b2e4c0281c0b02e7c53291a94d1d0cbff8883f8024f5142ee494ffbbd08807121017767aafcde9be00dcfd098715ebcf7f410daebc582fda69d24a28e9d0bc890d1";
-    uint32_t const GENESIS_NONCE = 22122;
+    uint32_t const GENESIS_NONCE = 10001;
   }
 
   namespace stagenet
